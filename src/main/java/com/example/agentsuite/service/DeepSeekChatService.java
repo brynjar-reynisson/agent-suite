8651e5c2e3f3ab946a00441569ce184a51179481
brynjar-reynisson <breynisson@gmail.com>
2026-06-11 09:55:01 +0000

    fix: add @Service to DeepSeekChatService for Spring bean injection

--- src/main/java/com/example/agentsuite/service/DeepSeekChatService.java
+++ src/main/java/com/example/agentsuite/service/DeepSeekChatService.java
@@ -4,7 +4,9 @@ import dev.langchain4j.model.chat.ChatModel;
 import dev.langchain4j.model.openai.OpenAiChatModel;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
 
+@Service
 public class DeepSeekChatService extends AbstractLangChain4jChatService {
 
     private final String apiKey;
